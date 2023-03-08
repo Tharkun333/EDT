28cd043e31a8cc67a5543b13ea548c31e1d74c6a
Guigui laverge <glavergne001@iutbayonne.univ-pau.fr>
2023-03-08 09:39:18 +0100

    erreur corrigée sur le crud

--- src/Controller/Admin/SalleCrudController.php
+++ src/Controller/Admin/SalleCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Salle;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 
 class SalleCrudController extends AbstractCrudController
 {
@@ -12,14 +13,11 @@ class SalleCrudController extends AbstractCrudController
         return Salle::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            'numero',
+            AssociationField::new('cours'),
         ];
     }
-    */
 }
